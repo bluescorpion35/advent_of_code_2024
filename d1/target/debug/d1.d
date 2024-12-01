b09a3010b26a6430e220a174b96e0a1ee5badde2
Matthew Given <143132572+bluescorpion35@users.noreply.github.com>
2024-12-01 19:30:06 +0000

    day 1

--- d1/target/debug/d1.d
+++
@@ -1,1 +0,0 @@
-/home/bluescorpion35/advent_of_code/d1/target/debug/d1: /home/bluescorpion35/advent_of_code/d1/src/main.rs
--- d1/target/debug/deps/d1-1050759c1ac6d2e3.d
+++
@@ -1,5 +0,0 @@
-/home/bluescorpion35/advent_of_code/d1/target/debug/deps/d1-1050759c1ac6d2e3: src/main.rs
-
-/home/bluescorpion35/advent_of_code/d1/target/debug/deps/d1-1050759c1ac6d2e3.d: src/main.rs
-
-src/main.rs:
